0d41f1a69e0040ee386b8bde4a62bd9b6d1218d5
acidicoala <acidicoala@gmail.com>
2021-03-28 14:57:30 +0300

    Version bump

--- scripts/update_versions.py
+++ scripts/update_versions.py
@@ -28,7 +28,7 @@ def process_file(filePath: str, patterns: List[Tuple[str, str]]):
 if __name__ == '__main__':
 	VER_MAJOR = 1
 	VER_MINOR = 2
-	VER_PATCH = 0
+	VER_PATCH = 1
 	VER_REVISION = 0
 
 	process_file('version_info.py', [
